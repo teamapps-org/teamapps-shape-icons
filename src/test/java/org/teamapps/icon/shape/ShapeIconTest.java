b950a3e72780b249deb3de3fe37c1be35a90466d
Yann Massard <yamass@gmail.com>
2022-03-31 21:01:27 +0200

    License; fix problems with encoding/decoding using commas and rgb colors

--- src/test/java/org/teamapps/icon/shape/ShapeIconTest.java
+++ src/test/java/org/teamapps/icon/shape/ShapeIconTest.java
@@ -1,3 +1,22 @@
+/*-
+ * ========================LICENSE_START=================================
+ * TeamApps Shape Icon Library
+ * ---
+ * Copyright (C) 2022 TeamApps.org
+ * ---
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =========================LICENSE_END==================================
+ */
 package org.teamapps.icon.shape;
 
 import org.apache.commons.io.FileUtils;
@@ -12,19 +31,18 @@ class ShapeIconTest {
 
 	@Test
 	void testLoadIcon() throws IOException {
-
-		for (ShapeIcon icon : ShapeIcon.values()) {
+		for (ShapeIcon icon : ShapeIcon.allIcons()) {
 			icon = icon.withStyle(ShapeIconStyle.builder()
-							.setFillColor(Color.BEIGE.withAlpha(.5f))
-							.setStrokeColor(Color.MATERIAL_BLUE_900)
-							.setStrokeWidth(5)
-							.setShadow(true)
-							.setScaling(.5f)
-							.build());
-			IconResource iconResource = new ShapeIconLoader().loadIcon(icon, 24, (icn, ctx) -> null);
-			FileUtils.writeByteArrayToFile(new File(icon.getIconName() + ".svg"), iconResource.getBytes());
+					.setFillColor(Color.BEIGE.withAlpha(.5f))
+					.setStrokeColor(Color.MATERIAL_BLUE_900)
+					.setStrokeWidth(5)
+					.setShadow(true)
+					.setScaling(.5f)
+					.build());
+			String encoded = new ShapeIconEncoder().encodeIcon(icon, icon1 -> null);
+			ShapeIcon decodedIcon = new ShapeIconDecoder().decodeIcon(encoded, qualifiedEncodedIcon -> null);
+			IconResource iconResource = new ShapeIconLoader().loadIcon(decodedIcon, 24, (icn, ctx) -> null);
+			FileUtils.writeByteArrayToFile(new File(decodedIcon.getIconName() + ".svg"), iconResource.getBytes());
 		}
-
-
 	}
 }
